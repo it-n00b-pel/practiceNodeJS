d6c956fa3a75578c9b0daa22d73de4307ff0a2f2
1t-n00b <mummintrol@gmail.com>
2023-01-03 14:35:40 +0300

    init

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,14 @@
 "use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : {"default": mod};
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, "__esModule", {value: true});
+Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.get("/", (req, res) => {
-    let helloWorld = "Hello motherFucker!!!!";
+app.get('/', (req, res) => {
+    let helloWorld = 'Hello motherFucker!!!!';
     res.send(helloWorld);
 });
 app.listen(3000, function () {
-    console.log("Example app listening on port 3000!");
+    console.log('Example app listening on port 3000!');
 });
